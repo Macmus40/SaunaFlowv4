85b6931e15f3fd1923bb9bfb3aea0de62bde733c
Macmus40 <macmus40@gmail.com>
2025-11-08 20:35:08 +0100

    fix: Update Supabase anon key
    The Supabase anonymous key has been updated to reflect recent changes or rotations. This ensures continued authentication and access to Supabase services for the application.

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Dane do połączenia z Twoim projektem Supabase
 const supabaseUrl = 'https://ipdzreofebvkeizqpnhm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZHpyZW9mZWJ2a2VpenFwbmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3NzQxMTcsImV4cCI6MjAxNDM1MDExN30.8i9Yd8d21zP-584kM9fL-M62C2zQjMlMlZThYWnJHQ';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZHpyZW9mZWJ2a2VpenFwbmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg3NzQxMTcsImV4cCI6MjAxNDM1MDExN30.8i9Yd8d21zP-584kM9fL-M62C2zQjMlMlZThYWnJHQeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZHpyZW9mZWJ2a2VpenFwbmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MjExMzMsImV4cCI6MjA3NzQ5NzEzM30.n1aqL1O6KJf0xRnnjZL8g67adPemtuIW7AoACWUGpqY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
